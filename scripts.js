26f5a900f00f5be50ce118b928f4089abdf42943
MuushyRoom <ronancuaresma10@gmail.com>
2025-01-21 21:01:25 +0800

    Completed the initial version of the website. The current version includes the following changes: - Updated `index.html` with the final content and structure. - Revised `readME.txt` to reflect the latest project details and instructions. - Enhanced `scripts.js` with additional functionality and optimizations. - Improved `styles.css` for better layout and responsiveness.
    Future updates will focus on deploying the website to a real domain and adding new features to enhance user experience.

---
+++ scripts.js
@@ -0,0 +1,427 @@
+
+
+
+function startHack() {
+  let cliNum = Math.floor(Math.random() *3);
+  let hackNum = Math.floor(Math.random() * 21);
+  let cli1 = document.getElementById("cli_1");
+  let cli2 = document.getElementById("cli_2");
+  let cli3 = document.getElementById("cli_3");
+  let cli4 = document.getElementById("cli_4");
+let cli5 = document.getElementById("cli_5");
+
+
+  if(cliNum == 0){
+    //CLI 1
+ 
+    document.getElementById("cli_1").scrollTop = document.getElementById("cli_1").scrollHeight 
+    if(hackNum == 0){
+      cli1.textContent += "      ";
+    }else if(hackNum == 1){
+    
+      cli1.textContent += "Uploading payload.exe [███████████████████████████████████████] 100% \r\n";
+      cli1.textContent += "Executing payload...\r\n ";
+    }
+    else if(hackNum == 2){
+      cli1.textContent += "  Firewall Status: DISABLED  ";
+      cli1.textContent += "$ deploy --malware payload.exe --target 192.168.1.100 \r\n";
+    }
+    else if(hackNum == 3){
+      cli1.textContent += "[2095-24-23 69:23:24] [INFO] Initializing system...\r\n";
+    }
+    else if(hackNum == 4){
+      cli1.textContent += "[2095-24-23 69:23:24] [DEBUG] Loading module 'CoreEncryption'\r\n";
+    }else if(hackNum == 5){
+      cli1.textContent += "  [2095-24-23 69:23:24] [INFO] Establishing remote connection...  ";
+    }
+    else if(hackNum == 6){
+      cli1.textContent += "  [2095-24-23 69:23:24] [INFO] Connection terminated.  ";
+      cli1.textContent += "[!] System Compromised. Admin privileges obtained.\r\n";
+    }
+    else if(hackNum == 7){
+      cli1.textContent += "  Generating secure hash...  ";
+      cli1.textContent += "Network Speed: 500Mbps up / 700Mbps down\r\n";
+    }
+    else if(hackNum == 8){
+      cli1.textContent += "  Firewall Status: DISABLED  ";
+    }
+    else if(hackNum == 9){
+      cli1.textContent += "\r\n";
+      cli1.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli1.textContent += "\r\n";
+    }
+    else if(hackNum == 10){
+      cli1.textContent += "Network Speed: 500Mbps up / 700Mbps down\r\n";
+      cli1.textContent += "$ sudo initiate_system_scan --target 192.168.0.1\r\n";
+      cli1.textContent += "Scanning for vulnerabilities... \r\n";
+    }
+    else if(hackNum == 11){
+      cli1.textContent += "  ";
+    }
+    else if(hackNum == 12){
+      cli1.textContent += "  ";
+    }
+    else if(hackNum == 13){
+      cli1.textContent += "\r\n";
+      cli1.textContent += "RAM Usage: ████████████░░░░░░ 76%";
+      cli1.textContent += "\r\n";
+    }
+    else if(hackNum == 14){
+      cli1.textContent += "\r\n";
+    }
+    else if(hackNum == 15){
+      cli1.textContent += "[+] Port 22: Open\r\n";
+    }
+    else if(hackNum == 16){
+      cli1.textContent += "[+] Access gained! \r\n ";
+    }
+    else if(hackNum == 17){
+      cli1.textContent += "[+] Found SSH credentials: root:toor\r\n ";
+    }
+    else if(hackNum == 18){
+      cli1.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+    }
+    else if(hackNum == 19){
+      cli1.textContent += "[2095-24-23 69:23:24] [ALERT] Unauthorized access detected. Countermeasures activated.\r\n";
+    }
+    else if(hackNum == 20){
+      cli1.textContent += "\r\n";
+      cli1.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli1.textContent += "\r\n";
+    }
+  }
+
+
+   else if(cliNum == 1){
+    //CLI 2
+    document.getElementById("cli_2").scrollTop = document.getElementById("cli_2").scrollHeight 
+    if(hackNum == 0){
+      cli2.textContent += "      ";
+    }else if(hackNum == 1){
+      cli2.textContent += "$ deploy --malware payload.exe --target 192.168.1.100 \r\n";
+     
+      cli2.textContent += "Executing payload...\r\n ";
+    }
+    else if(hackNum == 2){
+      cli2.textContent += "[!] System Compromised. Admin privileges obtained.\r\n";
+    }
+    else if(hackNum == 3){
+      cli2.textContent += "[2095-24-23 69:23:24] [INFO] Initializing system...\r\n";
+    }
+    else if(hackNum == 4){
+      cli2.textContent += "[2095-24-23 69:23:24] [DEBUG] Loading module 'CoreEncryption'\r\n";
+    }else if(hackNum == 5){
+      cli2.textContent += "  [2095-24-23 69:23:24] [INFO] Establishing remote connection...  ";
+    }
+    else if(hackNum == 6){
+      cli2.textContent += "  [2095-24-23 69:23:24] [INFO] Connection terminated.  ";
+    }
+    else if(hackNum == 7){
+      cli2.textContent += "  Generating secure hash...  ";
+      cli2.textContent += "[+] Found SSH credentials: root:toor\r\n ";
+    }
+    else if(hackNum == 8){
+      cli2.textContent += "  Firewall Status: DISABLED  ";
+      cli2.textContent += "Uploading payload.exe [███████████████████████████████████████] 100% \r\n";
+    }
+    else if(hackNum == 9){
+      cli2.textContent += "\r\n";
+      cli2.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli2.textContent += "\r\n";
+    }
+    else if(hackNum == 10){
+      cli2.textContent += "Network Speed: 500Mbps up / 700Mbps down\r\n";
+     
+      cli2.textContent += "Scanning for vulnerabilities... \r\n";
+    }
+    else if(hackNum == 11){
+      cli2.textContent += "  ";
+    }
+    else if(hackNum == 12){
+      cli2.textContent += "  ";
+    }
+    else if(hackNum == 13){
+      cli2.textContent += "\r\n";
+      cli2.textContent += "RAM Usage: ████████████░░░░░░ 76%";
+      cli2.textContent += "\r\n";
+    }
+    else if(hackNum == 14){
+      cli2.textContent += "\r\n";
+    }
+    else if(hackNum == 15){
+      cli2.textContent += "[+] Port 22: Open\r\n";
+      cli2.textContent += "$ sudo initiate_system_scan --target 192.168.0.1\r\n";
+    }
+    else if(hackNum == 16){
+      cli2.textContent += "[+] Access gained! \r\n ";
+    }
+    else if(hackNum == 17){
+      
+    }
+    else if(hackNum == 18){
+      cli2.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+    }
+    else if(hackNum == 19){
+      cli2.textContent += "[2095-24-23 69:23:24] [ALERT] Unauthorized access detected. Countermeasures activated.\r\n";
+    }
+    else if(hackNum == 20){
+      cli2.textContent += "\r\n";
+      cli2.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli2.textContent += "\r\n";
+    }
+
+
+  }
+  else  if(cliNum == 2){
+    //CLI 3
+    document.getElementById("cli_3").scrollTop = document.getElementById("cli_3").scrollHeight 
+    if(hackNum == 0){
+      cli3.textContent += "      ";
+    }else if(hackNum == 1){
+      cli3.textContent += "Network Speed: 500Mbps up / 700Mbps down\r\n";
+     
+      cli3.textContent += "Uploading payload.exe [███████████████████████████████████████] 100% \r\n";
+      cli3.textContent += "Scanning for vulnerabilities... \r\n";
+      
+    }
+    else if(hackNum == 2){
+      cli3.textContent += "[!] System Compromised. Admin privileges obtained.\r\n";
+    }
+    else if(hackNum == 3){
+      cli3.textContent += "[2095-24-23 69:23:24] [INFO] Initializing system...\r\n";
+    }
+    else if(hackNum == 4){
+      cli3.textContent += "[2095-24-23 69:23:24] [DEBUG] Loading module 'CoreEncryption'\r\n";
+      cli3.textContent += "$ sudo initiate_system_scan --target 192.168.0.1\r\n";
+    }else if(hackNum == 5){
+      cli3.textContent += "  [2095-24-23 69:23:24] [INFO] Establishing remote connection...  ";
+    }
+    else if(hackNum == 6){
+      cli3.textContent += "  [2095-24-23 69:23:24] [INFO] Connection terminated.  ";
+    }
+    else if(hackNum == 7){
+      cli3.textContent += "  Generating secure hash...  ";
+    }
+    else if(hackNum == 8){
+      cli3.textContent += "  Firewall Status: DISABLED  ";
+    }
+    else if(hackNum == 9){
+      cli3.textContent += "\r\n";
+      cli3.textContent += "  Firewall Status: DISABLED  ";
+      cli3.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli3.textContent += "\r\n";
+    }
+    else if(hackNum == 10){
+      cli3.textContent += "$ deploy --malware payload.exe --target 192.168.1.100 \r\n";
+      
+      cli3.textContent += "Executing payload...\r\n ";
+    }
+    else if(hackNum == 11){
+      cli3.textContent += "  ";
+    }
+    else if(hackNum == 12){
+      cli3.textContent += "  ";
+    }
+    else if(hackNum == 13){
+      cli3.textContent += "\r\n";
+      cli3.textContent += "RAM Usage: ████████████░░░░░░ 76%";
+      cli3.textContent += "\r\n";
+    }
+    else if(hackNum == 14){
+      cli3.textContent += "\r\n";
+    }
+    else if(hackNum == 15){
+      cli3.textContent += "[+] Port 22: Open\r\n";
+      cli3.textContent += "  Generating secure hash...  ";
+      cli3.textContent += "[+] Access gained! \r\n ";
+    }
+    else if(hackNum == 16){
+      cli3.textContent += "[+] Access gained! \r\n ";
+    }
+    else if(hackNum == 17){
+      cli3.textContent += "[+] Found SSH credentials: root:toor\r\n ";
+    }
+    else if(hackNum == 18){
+      cli3.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+    }
+    else if(hackNum == 19){
+      cli3.textContent += "[2095-24-23 69:23:24] [ALERT] Unauthorized access detected. Countermeasures activated.\r\n";
+    }
+    else if(hackNum == 20){
+      cli3.textContent += "\r\n";
+      cli3.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+      cli3.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli3.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+      cli3.textContent += "\r\n";
+    }
+
+    
+  }
+  if(cliNum == 0 || cliNum == 2){
+    //CLI 4
+    document.getElementById("cli_4").scrollTop = document.getElementById("cli_4").scrollHeight 
+    if(hackNum == 0){
+      cli4.textContent += "      ";
+    }else if(hackNum == 1){
+   
+      cli4.textContent += "$ deploy --malware payload.exe --target 192.168.1.100 \r\n";
+      cli4.textContent += "Uploading payload.exe [███████████████████████████████████████] 100% \r\n";
+      cli4.textContent += "Executing payload...\r\n ";
+    }
+    else if(hackNum == 2){
+      cli4.textContent += "[!] System Compromised. Admin privileges obtained.\r\n";
+    }
+    else if(hackNum == 3){
+      cli4.textContent += "[2095-24-23 69:23:24] [INFO] Initializing system...\r\n";
+    }
+    else if(hackNum == 4){
+      cli4.textContent += "[2095-24-23 69:23:24] [DEBUG] Loading module 'CoreEncryption'\r\n";
+    }else if(hackNum == 5){
+      cli4.textContent += "  [2095-24-23 69:23:24] [INFO] Establishing remote connection...  ";
+    }
+    else if(hackNum == 6){
+      cli4.textContent += "  [2095-24-23 69:23:24] [INFO] Connection terminated.  ";
+    }
+    else if(hackNum == 7){
+      cli4.textContent += "  Generating secure hash...  ";
+    }
+    else if(hackNum == 8){
+      cli4.textContent += "  Firewall Status: DISABLED  ";
+    }
+    else if(hackNum == 9){
+      cli4.textContent += "\r\n";
+      cli4.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli4.textContent += "\r\n";
+    }
+    else if(hackNum == 10){
+      cli4.textContent += "Network Speed: 500Mbps up / 700Mbps down\r\n";
+      cli4.textContent += "$ sudo initiate_system_scan --target 192.168.0.1\r\n";
+      cli4.textContent += "Scanning for vulnerabilities... \r\n";
+    }
+    else if(hackNum == 11){
+      cli4.textContent += "  ";
+    }
+    else if(hackNum == 12){
+      cli4.textContent += "  ";
+    }
+    else if(hackNum == 13){
+      cli4.textContent += "\r\n";
+      cli4.textContent += "RAM Usage: ████████████░░░░░░ 76%";
+      cli4.textContent += "\r\n";
+    }
+    else if(hackNum == 14){
+      cli4.textContent += "\r\n";
+    }
+    else if(hackNum == 15){
+      cli4.textContent += "[+] Port 22: Open\r\n";
+    }
+    else if(hackNum == 16){
+      cli4.textContent += "[+] Access gained! \r\n ";
+    }
+    else if(hackNum == 17){
+      cli4.textContent += "[+] Found SSH credentials: root:toor\r\n ";
+    }
+    else if(hackNum == 18){
+      cli4.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+    }
+    else if(hackNum == 19){
+      cli4.textContent += "[2095-24-23 69:23:24] [ALERT] Unauthorized access detected. Countermeasures activated.\r\n";
+    }
+    else if(hackNum == 20){
+      cli4.textContent += "\r\n";
+      cli4.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli4.textContent += "\r\n";
+    }
+
+    
+  }
+   if(cliNum == 2){
+    //CLI 5
+    document.getElementById("cli_5").scrollTop = document.getElementById("cli_5").scrollHeight 
+    if(hackNum == 0){
+      cli5.textContent += "      ";
+    }else if(hackNum == 1){
+      cli5.textContent += "$ deploy --malware payload.exe --target 192.168.1.100 \r\n";
+      cli5.textContent += "Uploading payload.exe [███████████████████████████████████████] 100% \r\n";
+      cli5.textContent += "  [2095-24-23 69:23:24] [INFO] Establishing remote connection...  ";
+    }
+    else if(hackNum == 2){
+      cli5.textContent += "[!] System Compromised. Admin privileges obtained.\r\n";
+      cli5.textContent += "Executing payload...\r\n ";
+    }
+    else if(hackNum == 3){
+      cli5.textContent += "[2095-24-23 69:23:24] [INFO] Initializing system...\r\n";
+    }
+    else if(hackNum == 4){
+      cli5.textContent += "[2095-24-23 69:23:24] [DEBUG] Loading module 'CoreEncryption'\r\n";
+    }else if(hackNum == 5){
+      cli5.textContent += "  [2095-24-23 69:23:24] [INFO] Establishing remote connection...  ";
+    }
+    else if(hackNum == 6){
+      cli5.textContent += "  [2095-24-23 69:23:24] [INFO] Connection terminated.  ";
+    }
+    else if(hackNum == 7){
+      cli5.textContent += "  Generating secure hash...  ";
+    }
+    else if(hackNum == 8){
+      cli5.textContent += "  Firewall Status: DISABLED  ";
+    }
+    else if(hackNum == 9){
+      cli5.textContent += "\r\n";
+      cli5.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli5.textContent += "$ sudo initiate_system_scan --target 192.168.0.1\r\n";
+      cli5.textContent += "\r\n";
+    }
+    else if(hackNum == 10){
+      cli5.textContent += "Network Speed: 500Mbps up / 700Mbps down\r\n";
+      
+      cli5.textContent += "Scanning for vulnerabilities... \r\n";
+    }
+    else if(hackNum == 11){
+      cli5.textContent += "  ";
+    }
+    else if(hackNum == 12){
+      cli5.textContent += "  ";
+    }
+    else if(hackNum == 13){
+      cli5.textContent += "\r\n";
+      cli5.textContent += "Scanning for vulnerabilities... \r\n";
+      cli5.textContent += "RAM Usage: ████████████░░░░░░ 76%";
+      cli5.textContent += "\r\n";
+    }
+    else if(hackNum == 14){
+      cli5.textContent += "\r\n";
+    }
+    else if(hackNum == 15){
+      cli5.textContent += "[+] Port 22: Open\r\n";
+    }
+    else if(hackNum == 16){
+      cli5.textContent += "[+] Access gained! \r\n ";
+      cli5.textContent += "RAM Usage: ███████████████░░ 80%";
+    }
+    else if(hackNum == 17){
+      cli5.textContent += "[+] Found SSH credentials: root:toor\r\n ";
+    }
+    else if(hackNum == 18){
+      cli5.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+    }
+    else if(hackNum == 19){
+      cli5.textContent += "[2095-24-23 69:23:24] [ALERT] Unauthorized access detected. Countermeasures activated.\r\n";
+    }
+    else if(hackNum == 20){
+      cli5.textContent += "\r\n";
+      cli5.textContent += "[!] Exploiting vulnerability CVE-2025-12345\r\n";
+      cli5.textContent += "System CPU Usage: ██████████░░░░░░░ 63%";
+      cli5.textContent += "\r\n";
+    }
+   
+  }
+
+  
+
+
+console.log(cliNum);
+  
+
+  }
+
